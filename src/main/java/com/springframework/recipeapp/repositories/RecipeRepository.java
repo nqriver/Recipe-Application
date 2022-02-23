875bb4b1fefab35947fbd533b0562c002fa2157f
nqriver <nqriver@gmail.com>
2022-02-24 00:57:44 +0100

    Adding repositories interfaces

---
+++ src/main/java/com/springframework/recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.springframework.recipeapp.repositories;
+
+
+import com.springframework.recipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+}
---
+++ src/main/java/com/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,8 @@
+package com.springframework.recipeapp.repositories;
+
+import com.springframework.recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
