c83e14564e8275329277a3e75b4e3ed8b5bdeef5
nqriver <nqriver@gmail.com>
2022-02-24 00:23:20 +0100

    Adding and setting relationships: Ingredient and UnitofMeasure POJOs

--- src/main/java/com/springframework/recipeapp/domain/Recipe.java
+++ src/main/java/com/springframework/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.springframework.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -16,6 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -101,5 +105,13 @@ public class Recipe {
 
     public void setNote(Note note) {
         this.note = note;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
