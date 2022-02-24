312884f23098d4860b184c769f3313022c2f105b
nqriver <nqriver@gmail.com>
2022-02-24 13:57:22 +0100

    Adding Query methods

--- src/main/java/com/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/com/springframework/recipeapp/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package com.springframework.recipeapp.controllers;
 
+import com.springframework.recipeapp.domain.Category;
+import com.springframework.recipeapp.domain.UnitOfMeasure;
+import com.springframework.recipeapp.repositories.CategoryRepository;
+import com.springframework.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+
+        System.out.println("Cat Id is : " + categoryOptional.get().getId());
+        System.out.println("UoM Id is : " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
--- src/main/java/com/springframework/recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/com/springframework/recipeapp/domain/UnitOfMeasure.java
@@ -12,14 +12,14 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String description;
 
-    public String getName() {
-        return name;
+    public String getDescription() {
+        return description;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setDescription(String name) {
+        this.description = name;
     }
 
     public void setId(Long id) {
