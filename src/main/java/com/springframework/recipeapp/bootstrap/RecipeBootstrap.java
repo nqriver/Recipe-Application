f3a4ea1afd91219a33a72be1dec58acd9d12d613
nqriver <nqriver@gmail.com>
2022-02-27 23:49:45 +0100

    Fixing lazy-initialization problem with transactional annotation

--- src/main/java/com/springframework/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/springframework/recipeapp/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,7 @@ public class RecipeBootstrap implements CommandLineRunner {
         this.unitOfMeasureRepository = unitOfMeasureRepository;
     }
 
+    @Transactional
     @Override
     public void run(String... args) throws Exception {
         recipeRepository.saveAll(getRecipes());
