31091f7d4e118a48ea6a5c0fa6f003b504f08964
nqriver <nqriver@gmail.com>
2022-03-20 14:49:31 +0100

    Adding view for ingredients

---
+++ src/test/java/com/springframework/recipeapp/controllers/IngredientControllerTest.java
@@ -0,0 +1,55 @@
+package com.springframework.recipeapp.controllers;
+
+import com.springframework.recipeapp.commands.RecipeCommand;
+import com.springframework.recipeapp.services.RecipeService;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@ExtendWith(MockitoExtension.class)
+class IngredientControllerTest {
+
+    @Mock
+    private RecipeService recipeService;
+
+    @InjectMocks
+    private IngredientController controller;
+
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @SneakyThrows
+    @Test
+    void testListIngredients() {
+        //given
+        RecipeCommand recipeCommand = new RecipeCommand();
+
+        //when
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+}
